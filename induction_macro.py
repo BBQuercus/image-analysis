b1490f6a86256edc65855637976bbbb078603c70
BBQuercus <bastian@eichenbergers.ch>
2019-09-06 15:41:12 +0200

    updated on 20190906

--- induction_macro.py
+++
@@ -1,8 +0,0 @@
-time_points = [20, 20, 100]
-laser_intensities = [20, 100, 50]
-
-for i in range(3):
-    VV.Acquire.TimeLapse.TimePoints = time_points[i]
-    VV.Illumination.SetComponentSlider('Toptica-488_Laser488', laser_intensities[i])
-    VV.Acquire.Sequence.Start()
-    VV.Macro.Contro.WaitFor('VV.Acquire.IsRunning', '==', 'False')
